0a7a91ac6a7ccad8a8903559845ce29bee49ccbe
pashtet <web.shorohov@yandex.ru>
2022-11-16 21:58:26 +0200

    Установил Mobx

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-// root.render(
-//     <App />
-// );
+root.render(
+    <App />
+);
 
 const editor = new FigureEditor();
